32ac9dca558eea4e57efb31ccfc41fb9bfd5a9e0
kosuke9809 <uht0912k@outlook.com>
2025-09-23 13:32:27 +0900

    feat: db設定

---
+++ backend/infra/database/database.go
@@ -0,0 +1,44 @@
+package database
+
+import (
+	"fmt"
+	"snapShare/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Connect(databaseURL string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get sql.DB: %w", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+
+	return db, nil
+}
+
+func Migrate(db *gorm.DB) error {
+	err := db.AutoMigrate(
+		&models.Event{},
+		&models.Photo{},
+		&models.Session{},
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to migrate: %w", err)
+	}
+
+	// TODO: indexの追加
+	return nil
+}
